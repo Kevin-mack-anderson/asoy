b264eee31c0faa716f34342498457f3fe4e4276d
Kevin-mack-anderson <pelajar24fairuzyahdimayassa@gmail.com>
2026-05-09 21:02:38 +0700

    Refactor Ukt_Mahasiswa class: streamline payment and status check methods, improve variable naming

--- uktMahasiswa.py
+++ uktMahasiswa.py
@@ -1,24 +1,59 @@
+import os
+
+os.system("cls")
 class Ukt_Mahasiswa:
 
-  def __init__(self, nama_mhs, status_mhs, nominalBayar, NIM):
+  
+  def __init__(self, nama_mhs, status:0, nominalBayar, NIM):
     self.nama = nama_mhs
-    self.status = status_mhs
+    self.status = status
     self.nominal = nominalBayar
     self.nim = NIM
-
-
+  
   # Fungsi cek status
+  
+  def cek_status(self):
+    if self.status == 0:
+       print("ANDA BELUM BAYAR UKT")
+    elif self.status == self.nominal:
+       print("UKT ANDA SUDAH LUNAS")
+    else:
+      print("apalah")
+    
 
-  def cek_status(self, status):
-    if status == 0:
-      print(f"Anda Belum Melakukan Pembayaran UKT, Segera Lakukan!")
-    elif status == self.nominal:
-      print(f"Status UKT anda sudah terbayarkan!!")
   
   # Fungsi Bayar
-  def bayar(self, bayar):
+  def bayar(self):
+    bayar = 0
     autentikasi = int(input("Silahkan Masukan NIM Anda: "))
-    if autentikasi is not self.nim:
-      print("NIM Anda Tidak Valid!!")
-    else: 
-      transaksi = 
+    if autentikasi == self.nim:
+      bayar = int(input("Masukan nominal yang sesuai (100000) = "))
+      if bayar < self.nominal:
+        print("Kurang bos")
+      else:
+        hasil = self.nominal =- bayar
+        print("Pembayaran Berhasil!!") 
+        self.status += hasil
+      return hasil
+    
+
+mhs = Ukt_Mahasiswa("Ucup surucup", 0, 100000, 12123)
+# Pilihan
+while True:
+  
+  pilih = input("Mau pilih apa 1= cekstatus 2=bayar : ")
+  if pilih == "1":
+    mhs.cek_status()
+  elif pilih == "2":
+    mhs.bayar()
+
+  # exit
+  pilihan = input("Apa mau lagi?(y/n):")
+  if pilihan == "y":
+    continue
+  elif pilihan == "n":
+    break
+  else:
+    print("pilihan tidak ada")
+    break
+
